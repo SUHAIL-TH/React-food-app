30feed2e7b9aaf719104ad6d12a60affb389aa40
Suhail TH <suhailth17756@gmail.com>
2024-01-15 21:35:50 +0530

    store subscription added

--- utils/store.js
+++ utils/store.js
@@ -2,6 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import cartSlice from "./cartSlice";
 
 //this function is used to create a store this is came form raeact toolkit
+//this is the thing we want to subscribe
 const store=configureStore({
     reducer:{
         cart:cartSlice
