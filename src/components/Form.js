613046ddc52792d8e50244c2b70cda1f8f2189b9
Suhail TH <suhailth17756@gmail.com>
2024-01-24 19:57:37 +0530

    form data added

--- src/components/Form.js
+++ src/components/Form.js
@@ -10,6 +10,7 @@ function Form() {
       <label htmlFor="channel"  >Channel</label>
       <input type="text" id='channel' name='channel' />
       <button>Submit</button>
+      
     </div>
   )
 }
